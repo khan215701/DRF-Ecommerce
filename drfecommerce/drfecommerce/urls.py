6e5537b0883208d59cb3cec374e34295255a0810
Khan2406 <82305740+khan215701@users.noreply.github.com>
2023-03-14 13:15:30 +0530

    added drf spectacular in urls to routing and view to responses

--- drfecommerce/drfecommerce/urls.py
+++ drfecommerce/drfecommerce/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from drfecommerce.product import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 router = DefaultRouter()
 router.register(r"category",views.CategoryView, basename="category-list")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
-    path("api/", include(router.urls))
+    path("api/", include(router.urls)),
+    path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
+    path('api/schema/doc/', SpectacularSwaggerView.as_view(), name="schema_doc"),
 ]
