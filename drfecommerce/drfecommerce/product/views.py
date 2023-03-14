427cf207c2280b68c231e39d5d924897298a170f
Khan2406 <82305740+khan215701@users.noreply.github.com>
2023-03-14 18:44:51 +0530

    brand rest framework api and routing in url

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import viewsets
-from .serializers import CategorySerializer
-from .models import Category
+from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
+from .models import Category, Brand, Product
 from drf_spectacular.utils import extend_schema
 # Create your views here.
 
@@ -16,6 +16,18 @@ class CategoryView(viewsets.ViewSet):
     @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many = True)
+        return Response(serializer.data)
+    
+
+class BrandView(viewsets.ViewSet):
+    """
+    using viewsets to listing Brand
+    """
+    queryset = Brand.objects.all()
+    
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request):
+        serializer = BrandSerializer(self.queryset, many = True)
         return Response(serializer.data)
     
 
--- drfecommerce/drfecommerce/urls.py
+++ drfecommerce/drfecommerce/urls.py
@@ -3,8 +3,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from drfecommerce.product import views
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+
 router = DefaultRouter()
 router.register(r"category",views.CategoryView, basename="category-list")
+router.register(r"brand", views.BrandView, basename="brand-list")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
